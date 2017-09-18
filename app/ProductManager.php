8a147e495da7623575a3c531361d454ab6567239
eaveryanau <e.aweryanau@gmail.com>
2017-09-18 12:39:29 +0300

    getProducts method done

--- app/ProductManager.php
+++ app/ProductManager.php
@@ -1,17 +1,50 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: eaveryanau
  * Date: 9/15/17
  * Time: 11:38 AM
  */
+Class ProductManager
+{
 
-Class ProductManager {
+    public static function getProducts()
+    {
 
-    public static function getProducts(){
+        $products = array();
+        $loop = new WP_Query([
+            'post_type' => ['product', 'product_variation'],
+            'posts_per_page' => -1
+        ]);
+        while ($loop->have_posts()) {
+            $loop->the_post();
+            $product = new WC_Product(get_the_ID());
+            $data = $product->get_data();
+            $categories_id = $product->get_category_ids();
+            $tags_id = $product->get_tag_ids();
+            $categories_name = [];
+            $categories_tags = [];
+            foreach ($categories_id as $cat_id) {
+                $categories_name[] = get_cat_name($cat_id);
+            }
+            foreach ($tags_id as $tag_id) {
+                $categories_tags[] = get_tag($tag_id)->name;
+            }
+            $date = $product->get_date_created()->date('Y-m-d');
+            $products[] = [
+                'id' => $data['id'],
+                'image' => $product->get_image(),
+                'name' => $data['name'],
+                'sku' => $data['sku'],
+                'stock_status' => $data['stock_status'],
+                'price' => $data['price'],
+                'categories' => $categories_name,
+                'tags' => $categories_tags,
+                'date' => $date
+            ];
+        }
 
-die('11');
-
-        return true;
+        return $products;
     }
 }
