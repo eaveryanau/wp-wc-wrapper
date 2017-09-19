ea8d8e9598db3de4f1d73f4edbca64894c0187d4
eaveryanau <e.aweryanov@gmail.com>
2017-09-19 18:10:28 +0300

    orderList, getOrder, updateOrder

--- app/OrderManager.php
+++ app/OrderManager.php
@@ -8,7 +8,69 @@
 
 Class OrderManager {
 
-    public static function getOrders(){
+	public static function getOrders() {
 
-    }
+		$orders = array();
+
+		$args = array(
+			'post_type'      => wc_get_order_types(),
+			'post_status'    => array_keys( wc_get_order_statuses() ),
+			'posts_per_page' => '-1'
+		);
+		$loop = new WP_Query( $args );
+
+		while ( $loop->have_posts() ) {
+			$loop->the_post();
+			$order = new WC_Order( get_the_ID() );
+
+			$date_format        = 'Y-m-d H:j:s';
+			$date_created       = ( $order->get_date_created() ) ? $order->get_date_created()->date( $date_format ) : null;
+			$date_last_modified = ( $order->get_date_modified() ) ? $order->get_date_modified()->date( $date_format ) : null;
+			$date_paid          = ( $order->get_date_paid() ) ? $order->get_date_paid()->date( $date_format ) : null;
+			$date_complete      = ( $order->get_date_completed() ) ? $order->get_date_completed()->date( $date_format ) : null;
+
+			$orders[] = [
+				'id'             => get_the_ID(),
+				'status'         => $order->get_status(),
+				'billing'        => [
+					'first_name' => $order->get_billing_first_name(),
+					'last_name'  => $order->get_billing_last_name(),
+					'email'      => $order->get_billing_email()
+				],
+				'ship'           => $order->get_shipping_total(),
+				'customer_note'  => $order->get_customer_note(),
+//			    'order_note'
+				'date'           => [
+					'created'       => $date_created,
+					'last_modified' => $date_last_modified,
+					'paid'          => $date_paid,
+					'complete'      => $date_complete
+				],
+				'total'          => $order->get_total(),
+				'payment_method' => $order->get_payment_method_title()
+			];
+		}
+
+		return $orders;
+	}
+
+	public static function getOrder( $id ) {
+
+		$order = WC_Order_Factory::get_order( $id );
+
+		return $order->get_data();
+	}
+
+	public static function updateOrder( $id, $data ) {
+
+		$order  = WC_Order_Factory::get_order( $id );
+		$helper = self::getOrder( $id );
+		foreach ( $helper as $prop => $val ) {
+			$setter = "set_$prop";
+			$order->{$setter}( $data[ $prop ] );
+		}
+
+		return $order->save();
+
+	}
 }
--- wp-wc-wrapper.php
+++ wp-wc-wrapper.php
@@ -9,55 +9,68 @@ Plugin URI:
 */
 
 // Define constant.
-define('BASE_HUB_API_URI', '/rest/v1/');
+define( 'BASE_HUB_API_URI', '/rest/v1/' );
 
 
 // Include methods for any entity.
-require_once('app/bootstrap.php');
+require_once( 'app/bootstrap.php' );
 
 // Check any requests.
-add_action('parse_request', 'urlHandler');
-function urlHandler()
-{
-    $request = [
-        'uri' => $_SERVER['REQUEST_URI'],
-        'method' => $_SERVER['REQUEST_METHOD'],
-        'data' => $_REQUEST
-    ];
-    if (strstr($request['uri'], BASE_HUB_API_URI)) {
-        $response = getResponseForHub($request);
-        wp_send_json($response);
-    }
+add_action( 'parse_request', 'urlHandler' );
+function urlHandler() {
+	$request = [
+		'uri'    => $_SERVER['REQUEST_URI'],
+		'method' => $_SERVER['REQUEST_METHOD'],
+		'data'   => $_REQUEST
+	];
+	if ( strstr( $request['uri'], BASE_HUB_API_URI ) ) {
+		$response = getResponseForHub( $request );
+		wp_send_json( $response );
+	}
 }
 
 // Processing requests.
-function getResponseForHub($request)
-{
-    switch ($request['uri']) {
+function getResponseForHub( $request ) {
+	switch ( $request['uri'] ) {
 
-        // Done.
-        case BASE_HUB_API_URI . 'products/index':
-            $response = ProductManager::getProducts();
-            break;
+		// Done.
+		case BASE_HUB_API_URI . 'products/index':
+			$response = ProductManager::getProducts();
+			break;
 
-        case (preg_match(',' . BASE_HUB_API_URI . 'products/view/([0-9]+$),', $request['uri'], $m) ? true : false) :
-            $response = ProductManager::getProduct($m[1]);
-            break;
+		case ( preg_match( ',' . BASE_HUB_API_URI . 'products/view/([0-9]+$),', $request['uri'], $m ) ? true : false ) :
+			$response = ProductManager::getProduct( $m[1] );
+			break;
 
-        case (preg_match(',' . BASE_HUB_API_URI . 'products/edit/([0-9]+$),', $request['uri'], $m) ? true : false) :
-            if($request['method'] == 'POST'){
-                $response = ProductManager::updateProduct($m[1], $request['data']);
-            }
-            else{
-                $response = ProductManager::getProduct($m[1]);
-            }
-            break;
+		case ( preg_match( ',' . BASE_HUB_API_URI . 'products/edit/([0-9]+$),', $request['uri'], $m ) ? true : false ) :
+			if ( $request['method'] == 'POST' ) {
+				$response = ProductManager::updateProduct( $m[1], $request['data'] );
+			} else {
+				$response = ProductManager::getProduct( $m[1] );
+			}
+			break;
 
-        default:
-            die('default');
-            $response = false;
-            break;
-    }
+		case BASE_HUB_API_URI . 'orders/index':
+			$response = OrderManager::getOrders();
+			break;
 
-    return $response;
+		case ( preg_match( ',' . BASE_HUB_API_URI . 'orders/view/([0-9]+$),', $request['uri'], $m ) ? true : false ) :
+			$response = OrderManager::getOrder( $m[1] );
+			break;
+
+		case ( preg_match( ',' . BASE_HUB_API_URI . 'orders/edit/([0-9]+$),', $request['uri'], $m ) ? true : false ) :
+			if ( $request['method'] == 'POST' ) {
+				$response = OrderManager::updateOrder( $m[1], $request['data'] );
+			} else {
+				$response = OrderManager::getOrder( $m[1] );
+			}
+			break;
+
+		default:
+			die( 'default' );
+			$response = false;
+			break;
+	}
+
+	return $response;
 }
